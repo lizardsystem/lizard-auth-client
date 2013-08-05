1386d8a4a7a51104574b1f0ac898a75c301b8610
Ernst Kuilder <ernst.kuilder@nelen-schuurmans.nl>
2013-08-05 15:18:47 +0200

    added middleware to log unauthenticated users in

--- lizard_auth_client/testsettings.py
+++ lizard_auth_client/testsettings.py
@@ -140,6 +140,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'lizard_auth_client.middleware.LoginRequiredMiddleware',
     )
 
 INSTALLED_APPS = (
