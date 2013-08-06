387c02fce3ad9d3c3e034ee7a2b8e1812e1c78ed
Ernst Kuilder <ernst.kuilder@nelen-schuurmans.nl>
2013-08-06 10:57:13 +0200

    changed inline documentation

--- lizard_auth_client/middleware.py
+++ lizard_auth_client/middleware.py
@@ -7,9 +7,7 @@ EXEMPT_URLS = ['accounts/login/', 'sso/local_login/']
 class LoginRequiredMiddleware:
     """
     Middleware that requires a user to be authenticated to view any page other
-    than LOGIN_URL. Exemptions to this requirement can optionally be specified
-    in settings via a list of regular expressions in LOGIN_EXEMPT_URLS (which
-    you can copy from your urls.py).
+    than EXEMPT_URLS (which you can copy from your urls.py).
 
     Requires authentication middleware and template context processors to be
     loaded. You'll get an error if they aren't.
