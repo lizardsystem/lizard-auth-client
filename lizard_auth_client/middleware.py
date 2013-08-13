68c42a2c67eb1470dac86c213a633ba6f7cb4e60
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-08-13 16:46:34 +0200

    better exempt_url handling

--- lizard_auth_client/middleware.py
+++ lizard_auth_client/middleware.py
@@ -1,10 +1,48 @@
+from re import compile
+from bisect import bisect_left
+
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from re import compile
 
-EXEMPT_URLS = ['accounts/login/', 'sso/local_login/']
 
-class LoginRequiredMiddleware:
+def string_has_any_prefix(prefix_list, some_string):
+    '''
+    Note: needs to be optimized for something better than O(N) efficiency.
+    '''
+    return any(some_string.startswith(p) for p in prefix_list)
+
+# url prefixes which shouldn't result in a redirect
+exempt_urls = [
+    'sso',
+    'api',
+    'admin',
+]
+
+# some urls are variable, because they are configurable via Django settings
+exempt_urls_from_settings = [
+    'STATIC_URL',
+    'MEDIA_URL',
+    'LOGIN_REDIRECT_URL',
+    'LOGIN_URL',
+]
+
+# copy these special urls from settings, if they have a value
+for key in exempt_urls_from_settings:
+    if hasattr(settings, key):
+        url = getattr(settings, key)
+        if url:
+            exempt_urls.append(url)
+
+# strip slashes on both sides
+exempt_urls = [url.strip('/') for url in exempt_urls]
+
+# strip whitespace
+exempt_urls = [url.strip() for url in exempt_urls]
+
+# strip out empty urls
+exempt_urls = [url for url in exempt_urls if url]
+
+class LoginRequiredMiddleware(object):
     """
     Middleware that requires a user to be authenticated to view any page other
     than EXEMPT_URLS (which you can copy from your urls.py).
@@ -13,13 +51,14 @@ class LoginRequiredMiddleware:
     loaded. You'll get an error if they aren't.
     """
     def process_request(self, request):
-        assert hasattr(request, 'user'), "The Login Required middleware\
-        requires authentication middleware to be installed. Edit your\
-        MIDDLEWARE_CLASSES setting to insert\
-        'django.contrib.auth.middlware.AuthenticationMiddleware'. If that doesn't\
-        work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes\
-        'django.core.context_processors.auth'."
+        assert hasattr(request, 'user'), '''The Login Required middleware
+        requires authentication middleware to be installed. Edit your
+        MIDDLEWARE_CLASSES setting to insert
+        'django.contrib.auth.middlware.AuthenticationMiddleware'. If that doesn't
+        work, ensure your TEMPLATE_CONTEXT_PROCESSORS setting includes
+        'django.core.context_processors.auth'.'''
+
         if not request.user.is_authenticated():
-            path = request.path_info.lstrip('/')
-            if path not in EXEMPT_URLS:
+            path = request.path_info.strip('/')
+            if not string_has_any_prefix(exempt_urls, path):
                 return HttpResponseRedirect(settings.LOGIN_URL)
