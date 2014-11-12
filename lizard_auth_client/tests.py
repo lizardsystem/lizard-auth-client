5d8940f2432d53b4f73d8b25d97e9fd5ad3ee850
Remco Gerlich <remco@gerlich.nl>
2014-11-12 14:38:41 +0100

    Refactor tests so that they work offline and have one test per test function

--- lizard_auth_client/tests.py
+++ lizard_auth_client/tests.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 import logging
 import pprint
 
+import mock
+
 from django.test import TestCase
 
 
@@ -14,36 +16,89 @@ class TestClient(TestCase):
     def test_authenticate_root(self):
         from lizard_auth_client import client
 
-        result = client.sso_authenticate_django('root', 'a')
-        self.assertEqual(result['username'], 'root')
+        with mock.patch('lizard_auth_client.client._do_post', return_value={
+                'success': True,
+                'user': {'username': 'root',
+                         'first_name': 'Willie',
+                         'last_name': 'Wortel',
+                         'email': 'noreply@example.com',
+                         'is_active': True,
+                         'is_staff': False,
+                         'is_superuser': False}}):
+            result = client.sso_authenticate_django('root', 'a')
+            self.assertEqual(result['username'], 'root')
 
-        result = client.sso_authenticate_unsigned_django('root', 'a')
-        self.assertEqual(result['username'], 'root')
+    def test_authenticate_unsiged_root(self):
+        from lizard_auth_client import client
+        with mock.patch('lizard_auth_client.client._do_post_unsigned',
+                        return_value={
+                            'success': True,
+                            'user': {'username': 'root',
+                                     'first_name': 'Willie',
+                                     'last_name': 'Wortel',
+                                     'email': 'noreply@example.com',
+                                     'is_active': True,
+                                     'is_staff': False,
+                                     'is_superuser': False}}):
 
-        def wrong_pw():
-            return client.sso_authenticate_django('root', 'wrong_password')
-        self.assertRaises(client.AutheticationFailed, wrong_pw)
+            result = client.sso_authenticate_unsigned_django('root', 'a')
+            self.assertEqual(result['username'], 'root')
+
+    def test_wrong_pw(self):
+        from lizard_auth_client import client
+        with mock.patch('lizard_auth_client.client._do_post', return_value={
+                'success': False,
+                'error': 'Wrong password'}):
+            def wrong_pw():
+                return client.sso_authenticate_django('root', 'wrong_password')
+            self.assertRaises(client.AutheticationFailed, wrong_pw)
+
+    def test_bad_url(self):
+        from lizard_auth_client import client
 
         def bad_url():
             return client.sso_authenticate('http://127.0.0.1:34577/', '', '',
                                            'root', 'a')
         self.assertRaises(client.CommunicationError, bad_url)
 
+    def test_bad_key(self):
+        from lizard_auth_client import client
+
         def bad_key():
             return client.sso_authenticate('http://127.0.0.1:8001/', 'asd',
                                            'asd', 'root', 'a')
         self.assertRaises(client.CommunicationError, bad_key)
 
+    def test_retrieve_user(self):
+        from lizard_auth_client import client
+
+        with mock.patch('lizard_auth_client.client._do_post', return_value={
+                'success': True,
+                'user': {'username': 'root'}}):
+            user_data = client.sso_get_user_django('root')
+            logger.debug(pprint.pformat(user_data))
+            self.assertEqual(user_data['username'], 'root')
+
     def test_populate_user(self):
         from lizard_auth_client import client
 
-        user_data = client.sso_get_user_django('root')
-        logger.debug(pprint.pformat(user_data))
-        self.assertEqual(user_data['username'], 'root')
+        with mock.patch('lizard_auth_client.client._do_post', return_value={
+                'success': True,
+                'user': {'username': 'root',
+                         'first_name': 'Willie',
+                         'last_name': 'Wortel',
+                         'email': 'noreply@example.com',
+                         'is_active': True,
+                         'is_staff': False,
+                         'is_superuser': False}}):
+            user = client.sso_populate_user_django('root')
+            self.assertEqual(user.username, 'root')
 
-        user = client.sso_populate_user_django('root')
-        self.assertEqual(user.username, 'root')
+    def test_unknown_user(self):
+        from lizard_auth_client import client
+        with mock.patch('lizard_auth_client.client._do_post', return_value={
+                'success': False, 'error': 'No such user'}):
 
-        def unknown_user():
-            return client.sso_populate_user_django('non_existing_username')
-        self.assertRaises(client.UserNotFound, unknown_user)
+            def unknown_user():
+                return client.sso_populate_user_django('non_existing_username')
+            self.assertRaises(client.UserNotFound, unknown_user)
