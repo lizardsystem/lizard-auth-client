ee981fbfaa2193818ea193b0677b68ceda768d11
Remco Gerlich <remco@gerlich.nl>
2014-11-12 12:03:28 +0100

    We have south migrations, so we should use south...

--- lizard_auth_client/testsettings.py
+++ lizard_auth_client/testsettings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'django_extensions',
+    'south',
 )
 
 CACHES = {
--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ install_requires = [
     'django-nose',
     'requests',
     'itsdangerous',
+    'south',
     ],
 
 tests_require = [
