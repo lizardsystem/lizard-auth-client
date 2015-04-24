578dccad5b03ef0b8545bbd005c8847462243ec6
Heleen van de Schraaf <heleen.vandeschraaf@nelen-schuurmans.nl>
2015-04-24 14:38:55 +0200

    Add support for Django 1.7.
    Unfortunately from now on, because there are projects using this package that
    depend on Django 1.4, two sets of migrations will need to be created for this
    package in the case of model changes: a South migration and a Django
    migration.
    
    * Update the Django requirement and move South dependency to `extras_require`.
      To make lizard_auth_client Django 1.7 compatible we need to follow these
      instructions:
      https://docs.djangoproject.com/en/1.7/topics/migrations/#libraries-third-party-apps
      Note that South is only necessary for projects using Django < 1.7.
    
    * Move South migrations to south_migrations folder.
      South 1.0 will always check south_migrations first before using the normal
      migrations folder.
      See: https://docs.djangoproject.com/en/1.7/topics/migrations/#libraries-third-party-apps
    
    * Add new Django-style migrations.

--- setup.py
+++ setup.py
@@ -6,41 +6,43 @@ long_description = '\n\n'.join([
     open('README.rst').read(),
     open('CREDITS.rst').read(),
     open('CHANGES.rst').read(),
-    ])
+])
 
 install_requires = [
-    'Django >= 1.4, < 1.7',
+    'Django >= 1.4',
     'django-extensions',
     'django-nose',
     'requests',
     'itsdangerous',
-    'south',
     'mock',
-    ],
+],
 
-tests_require = [
-    ]
+tests_require = []
 
-setup(name='lizard-auth-client',
-      version=version,
-      description="A client for lizard-auth-server",
-      long_description=long_description,
-      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=['Programming Language :: Python',
-                   'Framework :: Django',
-                   ],
-      keywords=[],
-      author='Erik-Jan Vos, Remco Gerlich',
-      author_email='remco.gerlich@nelen-schuurmans.nl',
-      url='http://www.nelen-schuurmans.nl/',
-      license='MIT',
-      packages=['lizard_auth_client'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      tests_require=tests_require,
-      extras_require={'test': tests_require},
-      entry_points={
-          'console_scripts': [
-          ]},
-      )
+setup(
+    name='lizard-auth-client',
+    version=version,
+    description="A client for lizard-auth-server",
+    long_description=long_description,
+    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=['Programming Language :: Python',
+                 'Framework :: Django',
+                 ],
+    keywords=[],
+    author='Erik-Jan Vos, Remco Gerlich',
+    author_email='remco.gerlich@nelen-schuurmans.nl',
+    url='http://www.nelen-schuurmans.nl/',
+    license='MIT',
+    packages=['lizard_auth_client'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'test': tests_require,
+        'south': ['south >= 1.0'],
+    },
+    entry_points={
+        'console_scripts': []
+    },
+)
