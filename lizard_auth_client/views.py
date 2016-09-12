413a59e8a29a0e17f94984cdf2d2f5ad256b7ada
Reinout van Rees <reinout@vanrees.org>
2016-09-12 17:04:11 +0200

    Changed sso login url to the new, more correct, name

--- lizard_auth_client/views.py
+++ lizard_auth_client/views.py
@@ -139,7 +139,7 @@ class JWTLoginView(View):
             })
 
         # Build an absolute URL pointing to the SSO server out of it.
-        url = urljoin(settings.SSO_SERVER_PUBLIC_URL_V2, 'authorize/')
+        url = urljoin(settings.SSO_SERVER_PUBLIC_URL_V2, 'authenticate/')
         url_with_params = '%s?%s' % (url, query_string)
         return HttpResponseRedirect(url_with_params)
 
