efc4221a325371e989a3e27bd11e9f0ef5bfd446
byrman <carsten.byrman@nelen-schuurmans.nl>
2019-12-11 11:04:45 +0100

    Log 403 by SSO at level INFO.

--- lizard_auth_client/client.py
+++ lizard_auth_client/client.py
@@ -228,6 +228,7 @@ def sso_authenticate_v2(sso_server_api_start_url, sso_key, sso_secret,
 
     """
     try:
+        r = None
         payload = {
             # Identifier for this site
             'iss': sso_key,
@@ -247,9 +248,14 @@ def sso_authenticate_v2(sso_server_api_start_url, sso_key, sso_secret,
             r.raise_for_status()
         return r.json()['user']
 
-    except:
-        logger.exception(
-            "Exception occurred while asking SSO to check credentials")
+    except Exception as e:
+        if r is not None and r.status_code == 403:
+            # A wrong password error shouldn't appear in Sentry,
+            # so log 403 at level INFO.
+            logger.info(e)
+        else:
+            logger.exception(
+                "Exception occurred while asking SSO to check credentials")
         raise
 
 
